d28a2cb45e67bb669ed10e40bdd920e428c48f4d
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2020-05-25 08:58:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+// camera test
+
+window.onload = () => {
+    getStream();
+};
+
+let stream;
+
+const getUserMedia = (options, successCallback, failureCallback) => {
+    let api = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+    if(api){
+        return api.bind(navigator)(options, successCallback, failureCallback);
+    }
+}
+
+const getStream = () =>  {
+    if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+      alert('User Media API not supported.');
+      return;
+    }
+    
+    var constraints = {
+      video: true
+    };
+  
+    getUserMedia(constraints, (stream) => {
+      var mediaControl = document.querySelector('video');
+      if ('srcObject' in mediaControl) {
+        mediaControl.srcObject = stream;
+      } else if (navigator.mozGetUserMedia) {
+        mediaControl.mozSrcObject = stream;
+      } else {
+        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+      }
+      theStream = stream;
+    }, (err) => {
+      alert('Error: ' + err);
+    });
+  }
+  
+  function takePhoto() {
+    if (!('ImageCapture' in window)) {
+      alert('ImageCapture is not available');
+      return;
+    }
+    
+    if (!theStream) {
+      alert('Grab the video stream first!');
+      return;
+    }
+    
+    let theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+  
+    theImageCapturer.takePhoto()
+      .then(blob => {
+        let theImageTag = document.querySelector('.photo');
+        theImageTag.src = URL.createObjectURL(blob);
+      })
+      .catch(err => alert('Error: ' + err));
+    }
