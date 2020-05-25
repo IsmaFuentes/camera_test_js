ebb9fb8c38ab3e1b0a2e2e40e32e37a86269ef19
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2020-05-25 09:26:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -25,19 +25,20 @@ const getStream = () =>  {
     }
     
     var constraints = {
-      video: true,
-      width: { 
-        min: 1280,
-        ideal: 1920,
-        max: 2560,
-      },
-      height: {
-        min: 720,
-        ideal: 1080,
-        max: 1440
-      },
-      facingMode: { 
-          exact: "environment" 
+      video: {
+        width: { 
+            min: 1280,
+            ideal: 1920,
+            max: 2560,
+          },
+          height: {
+            min: 720,
+            ideal: 1080,
+            max: 1440
+          },
+          facingMode: { 
+              exact: 'environment'
+          }
       }
     };
   
