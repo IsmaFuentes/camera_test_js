ae91b9bff587fbbe8af011edc121f0ca6fd34df7
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2020-05-26 12:52:23 +0200

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,37 @@
+window.onload = () => {
+  navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+  document.querySelector('#capture').onclick = () => { takePhoto() };
+}
+
+const video = document.querySelector('video');
+const canvas = window.canvas = document.querySelector('canvas');
+
+canvas.width = 480;
+canvas.height = 360;
+
+const takePhoto = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+  //canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+}
+
+const constraints = {
+  audio: false,
+  video: {
+    facingMode: { 
+      exact: 'environment'
+    }
+  }
+};
+
+const handleSuccess = (stream) => {
+  window.stream = stream;
+  video.srcObject = stream;
+}
+
+const handleError = (error) => {
+  console.log('Get user media error: ', error.message, error.name);
+}
