50187b1ba05a187f108c79e3e9644da553d6456d
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2020-05-26 10:13:46 +0200

    Add files via upload

--- canvas/script.js
+++ canvas/script.js
@@ -1,28 +1,34 @@
+window.onload = () => {
+  navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+  document.querySelector('button').onclick = () => { takePhoto() };
+}
+
 // Put variables in global scope to make them available to the browser console.
 const video = document.querySelector('video');
 const canvas = window.canvas = document.querySelector('canvas');
+
 canvas.width = 480;
 canvas.height = 360;
 
-const button = document.querySelector('button');
-button.onclick = function() {
+const takePhoto = () => {
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-};
+}
 
 const constraints = {
   audio: false,
-  video: true
+  video: true,
+  facingMode: { 
+    exact: 'environment'
+  }
 };
 
-function handleSuccess(stream) {
-  window.stream = stream; // make stream available to browser console
+const handleSuccess = (stream) => {
+  window.stream = stream;
   video.srcObject = stream;
 }
 
-function handleError(error) {
-  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+const handleError = (error) => {
+  console.log('Get user media error: ', error.message, error.name);
 }
-
-navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
