c0dfc43cecb58889b249eae903343faf28439e3a
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2020-05-26 10:28:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -18,63 +18,62 @@ const getUserMedia = (options, successCallback, failureCallback) => {
 }
 
 const getStream = () =>  {
-    if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-      console.log('User Media API not supported.');
-      return;
-    }
-    
-    const constraints = {
-      video: {
-        width: { 
-            min: 1280,
-            ideal: 1920,
-            max: 2560,
-          },
-          height: {
-            min: 720,
-            ideal: 1080,
-            max: 1440
-          },
-          facingMode: { 
-              exact: 'environment'
-          }
-      }
-    };
-  
-    getUserMedia(constraints, (stream) => {
-      var mediaControl = document.querySelector('#video');
-      if ('srcObject' in mediaControl) {
-        mediaControl.srcObject = stream;
-      } else if (navigator.mozGetUserMedia) {
-        mediaControl.mozSrcObject = stream;
-      } else {
-        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      }
-      theStream = stream;
-    }, (err) => {
-      console.log('Error: ' + err);
-    });
+  if (!navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+    console.log('User Media API not supported.');
+    return;
   }
+    
+  const constraints = {
+    video: {
+      width: { 
+          min: 1280,
+          ideal: 1920,
+          max: 2560,
+        },
+        height: {
+          min: 720,
+          ideal: 1080,
+          max: 1440
+        },
+        facingMode: { 
+          exact: 'environment'
+        }
+    }
+  };
+    
+  getUserMedia(constraints, (stream) => {
+    let mediaControl = document.querySelector('#video');
+    if ('srcObject' in mediaControl) {
+      mediaControl.srcObject = stream;
+    } else if (navigator.mozGetUserMedia) {
+      mediaControl.mozSrcObject = stream;
+    } else {
+      mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+    }
+      theStream = stream;
+    }, (err) => { console.log('Error: ' + err);
+  });
+}
   
 const takePhoto = () => {
-    if (!('ImageCapture' in window)) {
-      console.log('ImageCapture is not available');
-      return;
-    }
+  if (!('ImageCapture' in window)) {
+    console.log('ImageCapture is not available');
+    return;
+  }
     
-    if (!theStream) {
-      console.log('Grab the video stream first!');
-      return;
-    }
+  if (!theStream) {
+    console.log('Grab the video stream first!');
+    return;
+  }
     
-    let theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+  let theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
   
-    theImageCapturer.takePhoto()
-      .then(blob => {
-        let theImageTag = document.querySelector('.photo');
-        theImageTag.src = URL.createObjectURL(blob);
-      })
-      .catch(err => console.log('Error: ' + err));
+  theImageCapturer.takePhoto()
+  .then(blob => {
+    let theImageTag = document.querySelector('.photo');
+    theImageTag.src = URL.createObjectURL(blob);
+  })
+  .catch(err => console.log('Error: ' + err));
 }
 
