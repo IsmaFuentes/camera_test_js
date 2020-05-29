236f71e8e305f61426921c48bda047c0e9b468f2
Ismael Fuentes <32370252+IsmaFuentes@users.noreply.github.com>
2020-05-29 11:14:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 window.onload = () => {
-  navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(error => console.log(error));
-  document.querySelector('button').onclick = () => { takePhoto() };
+  navigator.mediaDevices.getUserMedia(constraints).then(
+    handleSuccess
+  ).catch(
+    error => console.log(error)
+  );
 }
+
 
 const video = document.querySelector('video');
 const img = document.querySelector("#myImage");
+const slider = document.querySelector("#mySlider");
 
 const takePhoto = () => {
   const imageCapture = window.imageCapture;
@@ -15,6 +20,8 @@ const takePhoto = () => {
     }).catch(
       err => console.log(err)
     );
+  }else{
+    alert("Image capture is not suported")
   }
 }
 
@@ -29,10 +36,22 @@ const constraints = {
 };
 
 const handleSuccess = (stream) => {
+  // basic config
   window.stream = stream;
   video.srcObject = stream;
   const mediaStreamStrack = stream.getVideoTracks()[0];
   const imageCapture = new ImageCapture(mediaStreamStrack);
   window.imageCapture = imageCapture;
+
+  // slider
+  const capabilities = mediaStreamStrack.getCapabilities();
+  const settings = mediaStreamStrack.getSettings();
+  if(capabilities.zoom){
+    slider.min = capabilities.zoom.min;
+    slider.max = capabilities.zoom.max;
+    slider.step = capabilities.zoom.step;
+    slider.value = settings.zoom;
+  }
 }
 
+document.querySelector('button').addEventListener("click", takePhoto);
